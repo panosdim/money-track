1c171ecccf85f5c64aa8c16b74ea6b6f83933428
Panagiotis Dimopoulos <panosdim@gmail.com>
2017-07-06 15:20:27 +0300

    Fix some bugs in income editor. Expense tab almost ready. Only filters are missing.

---
+++ php/delete_expense.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+// If not Login exit
+if (!isset($_SESSION['userId'])) {
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'You are not logged in.'
+    ]);
+    exit();
+}
+
+require_once 'database.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+// Check if it is an update or new entry
+if (isset($data['id'])) {
+    $stmt = $db->prepare(
+        'DELETE FROM `expenses` WHERE `id` = ?'
+    );
+
+    if ($stmt->execute([$data['id']])) {
+        echo json_encode([
+            "status"  => "success",
+            "message" => "Expense was deleted successfully.",
+        ]);
+    } else {
+        // DB interaction was not successful. Inform user with message.
+        echo json_encode([
+            "status"  => "error",
+            "message" => "Problem executing statement in DB. Try again later."
+        ]);
+    }
+} else {
+    // DB interaction was not successful. Inform user with message.
+    echo json_encode([
+        "status"  => "error",
+        "message" => "Problem executing statement in DB. Try again later."
+    ]);
+}
